eaf1b028b65166b66703a7272c193d02eb18059a
ludvigm <lm222ix@student.lnu.se>
2017-03-22 08:43:14 +0100

    Soon...

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 'use strict';
 const discord = require('discord.js');
+const async = require('async');
+const Promise = require('promise');
+
 require('dotenv').config();
 
 
@@ -21,25 +24,73 @@ var voicechannels = [];
 bot.on('ready', () => {
     console.log('I am ready!');
     var channels = bot.channels;
-    channels.forEach(function(channel) {
-        if(channel.type === 'voice') {
+    channels.forEach(function (channel) {
+        if (channel.type === 'voice') {
             voicechannels.push(channel);
-        } else if(channel.type === 'text') {
+        } else if (channel.type === 'text') {
             textchannels.push(channel);
         }
     });
+    currentUsersStatus();
+
 });
+
+function currentUsersStatus() {
+
+    var currUsers = bot.users;
+    var offline = [];
+    var online = [];
+    var idle = [];
+    var dnd = [];
+    var ingame = [];
+
+    new Promise(function (resolve, reject) {
+        currUsers.forEach(function (user) {
+            switch (user.presence.status) {
+                case 'online':
+                    online.push(user);
+                    if (user.presence.game) {
+                        ingame.push({
+                            user: user,
+                            game: user.presence.game
+                        })
+                    }
+                    break;
+                case 'offline':
+                    offline.push(user);
+                    break;
+                case 'idle':
+                    idle.push(user);
+                    break;
+                case 'dnd':
+                    dnd.push(user);
+                    break;
+            }
+        });
+        resolve();
+    }).then(function () {
+        console.log(offline.length, online.length, idle.length, dnd.length, ingame.length);
+        ingame.forEach(function(game) {
+            console.log(game);
+        })
+    })
+}
+
 
 //Message listener
 bot.on('message', message => {
     // if the message is "what is my avatar",
     console.log(message.channel.name);
-    if (message.content === 'what is my avatar') {
+    if (message.content === '!avatar') {
         // send the user's avatar URL
         message.reply(message.author.avatarURL);
     }
 
-    if(message.content === 'who is the man?') {
+    if (message.content === '!test') {
+        console.log(message.author.client.presences)
+    }
+
+    if (message.content === '!game') {
         message.reply()
     }
 });
