36294d55e175e96776fbb39cb33f13cbd7431557
ludvigm <lm222ix@student.lnu.se>
2017-03-29 14:43:03 +0200

    dates

--- models/ScheduledEvent.js
+++ models/ScheduledEvent.js
@@ -3,16 +3,14 @@
 let mongoose = require('mongoose');
 
 let eventSchema = mongoose.Schema({
-        title: {type: String, required: true},
-        code: {type: String, required: true},
-        description : {type: String, required: false},
-        createdBy: {type: String}
+        description: {type: String, required: true},
+        date: {type: Date, required: true},
     },
     {
         timestamps: true
     }
 );
 
-let Snippet = mongoose.model('ScheduledEvent', eventSchema);
+let ScheduledEvent = mongoose.model('ScheduledEvent', eventSchema);
 
 module.exports = ScheduledEvent;
